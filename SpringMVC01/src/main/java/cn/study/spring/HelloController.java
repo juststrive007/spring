c45b24c21e58ee2ab9814493d37c7c3fc83b5552
DESKTOP-RINLN9T\wm <juststrive007@gmail.com>
2021-04-10 16:58:19 +0800

    Study：Spring MVC

--- SpringMVC01/src/main/java/cn/study/spring/HelloController.java
+++ SpringMVC01/src/main/java/cn/study/spring/HelloController.java
@@ -2,12 +2,14 @@ package cn.study.spring;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
 
 @Controller
 public class HelloController {
     @RequestMapping("hello.do")
     public String showHello(){
         System.out.println("HelloController.showHello()");
-        return "hello";
+        return "welcome";
+
     }
 }
