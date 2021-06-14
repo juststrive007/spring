88a35f27f1041a082ec61adcaef1ae45d0a5915c
DESKTOP-RINLN9T\wm <juststrive007@gmail.com>
2021-06-14 19:48:38 +0800

    Study：2021-6-14：mybatis 增删改查

--- MyBatis/src/main/java/cn/study/mybatis/UserMapper.java
+++ MyBatis/src/main/java/cn/study/mybatis/UserMapper.java
@@ -1,7 +1,27 @@
 package cn.study.mybatis;
 
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
 public interface UserMapper {
 
     Integer addnew(User user);
-    
+
+    Integer deleteById(Integer id);
+
+    Integer deleteByIds(List<Integer>ids);
+
+    Integer updatePassword(String password);
+
+    Integer updateEmailById(@Param("id") Integer id,@Param("email") String email);
+
+    Integer count();
+
+    User findById(Integer id);
+
+    List<User> findAll();
+
+    User findOld();
+
 }
--- MyBatis/src/test/java/cn/study/mybatis/Tests.java
+++ MyBatis/src/test/java/cn/study/mybatis/Tests.java
@@ -8,6 +8,8 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Tests {
     ClassPathXmlApplicationContext ac;
@@ -33,12 +35,73 @@ public class Tests {
         System.out.println("int:" +integer);
     }
 
+    @Test
+    public void deletebyId(){
+        Integer id=1;
+        Integer rows = userMapper.deleteById(id);
+        System.out.println("rows"+rows);
+    }
+    @Test
+    public void deleteByIds(){
+        List<Integer> ids = new ArrayList<>();
+        ids.add(1);
+        ids.add(2);
+        Integer rows = userMapper.deleteByIds(ids);
+        System.out.println("rows:"+rows);
+    }
+
+    @Test
+    public void updatepassword(){
+        String password="wangmeng";
+        Integer rows = userMapper.updatePassword(password);
+        System.out.println("rows"+rows);
+    }
+
+    @Test
+    public void updateEmailById(){
+        String email="juststrive@gmail.com";
+        Integer rows = userMapper.updateEmailById(1,email);
+        System.out.println(rows);
+    }
+
+    @Test
+    public void count(){
+        Integer rows = userMapper.count();
+        System.out.println("rows"+rows);
+    }
+
+    @Test
+    public void findById(){
+        User user = userMapper.findById(1);
+        System.out.println(user);
+    }
+
+    @Test
+    public  void findAll(){
+        List<User> users = userMapper.findAll();
+        System.out.println("user number"+users.size());
+        for(User u :users   ) {
+            System.out.println(u);
+        }
+
+    }
+
+    @Test
+    public void findOld(){
+        User olduser= userMapper.findOld();
+        System.out.println(olduser);
+    }
+
+
+
     @Before
     public void doBefore(){
         ac = new ClassPathXmlApplicationContext(
                 "spring-dao.xml");
         userMapper = ac.getBean("userMapper",UserMapper.class);
     }
+
+
 
     @After
     public void doAfter(){
